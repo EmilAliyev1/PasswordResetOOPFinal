71e39cca159d253e327cb937e986586ad1094c80
Emil Aliyev <aliyev.emiw@proton.me>
2026-06-05 17:42:08 +0300

    minor change

--- Services/PasswordGenerator.cs
+++ Services/PasswordGenerator.cs
@@ -15,7 +15,7 @@ public class PasswordGenerator
     
     public string GenerateTargetPassword()
     {
-        int passwordLength = _random.Next(4, 7);
+        int passwordLength = _random.Next(4, 6);
 
         StringBuilder passwordBuilder = new StringBuilder(passwordLength);
 
